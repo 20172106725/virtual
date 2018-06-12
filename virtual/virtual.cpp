493e3eeb934406f8e1d5d56404bb34978ee94740
20172106725 <15134008212@163.com>
2018-06-12 14:54:21 +0800

    Initial commit

---
+++ virtual/virtual.cpp
@@ -0,0 +1,40 @@
+// virtual.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+class A
+{
+public :
+	virtual void print() { cout << "A::print" << endl; }
+};
+class B :public A
+{
+public:
+	virtual void print() { cout << "B::print" << endl; }
+};
+class D :public A
+{
+public :
+	virtual void print() { cout << "D::print" << endl; }
+};
+class E :public B
+{
+	virtual void print() { cout << "E::print" << endl; }
+};
+
+int main()
+{
+	A a; B b; D d; E e;
+	A*pa = &a; B*pb = &b;
+	pa->print();
+	pa = pb;
+	pa->print();
+	pa = &d;
+	pa->print();
+	pa = &e;
+	pa->print();
+    return 0;
+}
+
